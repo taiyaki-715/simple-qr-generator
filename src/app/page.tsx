6738f70a8d2acadef67c28e8480f052ab961bdbe
taiyaki-715 <taiki-ishikawa@r09.jp>
2023-02-14 15:27:58 +0900

    ダウンロード機能追加・レスポンシブ対応

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,22 +7,25 @@ export default function Home() {
     const [value, setValue] = useState("");
 
   return (
-    <div className={"flex flex-col w-screen p-8 gap-8 items-center bg-gray-100 flex-1"}>
+    <div className={"flex flex-col md:flex-row md:items-start w-screen p-8 gap-8 items-center bg-gray-100 flex-1"}>
       <textarea
           onChange={(event) => setValue(event.target.value)}
-          className={"rounded w-full resize-none p-2"}
+          className={"rounded w-full resize-none p-2 h-full"}
           rows={5}
           placeholder={"Type here to make QR code"}
       ></textarea>
+        <div className={"flex flex-col w-full gap-8 items-center"}>
         <QRCode
             value={value}
             className={"w-full bg-white rounded py-8" + (value ? "" : " blur")}
         />
-        <button
-            className={"flex justify-center items-center gap-2 text-white bg-green-500 rounded p-4 tracking-widest font-bold uppercase w-full"}
+        <a
+            className={"flex justify-center items-center gap-2 text-white bg-green-500 rounded p-4 tracking-widest font-bold uppercase w-full hover:bg-white hover:text-green-500 hover:border hover:border-green-500 transition"}
+            href={value ? "https://api.qrserver.com/v1/create-qr-code/?size=300x300&data="+(value) : ""}
         >
             <FaDownload />Download
-        </button>
+        </a>
+        </div>
     </div>
   )
 }
